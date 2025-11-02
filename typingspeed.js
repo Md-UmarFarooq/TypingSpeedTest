67deb135c23a244e0542278f833356cece346c5d
Md-UmarFarooq <163745069+Md-UmarFarooq@users.noreply.github.com>
2025-11-02 23:07:50 +0530

    Update typingspeed.js

--- typingspeed.js
+++ typingspeed.js
@@ -18,7 +18,7 @@ var correctWords=0;
 var totalWordsTyped=0;
 var ComputedInput="";
 
-
+//test
 
 var characters=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var spaces=[1,2,3,4,5,6,7,8];
@@ -206,4 +206,5 @@ function message(){
 }
 
 inputText.addEventListener("input",callForMobile);
+
 
